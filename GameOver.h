8a4f3cd2191b98c9fae2e7842fb53d2eacaa3652
adrianhihi <adrianhihi@users.noreply.github.com>
2015-02-06 02:20:38 -0800

    Create GameOver.h

---
+++ GameOver.h
@@ -0,0 +1,18 @@
+#ifndef __planegame__GameOver__
+#define __planegame__GameOver__
+
+#include <cocos2d.h>
+USING_NS_CC;
+
+class GameOver : public cocos2d::Layer
+{
+public:
+	CREATE_FUNC(GameOver);
+	bool init();
+	static cocos2d::Scene * createScene();
+	void menuCallBack(cocos2d::Ref * pSender);
+};
+
+
+
+#endif /* defined(__planegame__GameOver__) */
