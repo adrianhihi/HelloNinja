f407f218fd78e478f3d07770da630c420cadc395
adrianhihi <adrianhihi@users.noreply.github.com>
2015-02-06 02:20:59 -0800

    Create GameOver.cpp

---
+++ GameOver.cpp
@@ -0,0 +1,36 @@
+
+
+#include "GameOver.h"
+#include "HelloWorldScene.h"
+
+Scene * GameOver::createScene() {
+	auto scene = Scene::create();
+	auto layer = GameOver::create();
+	scene->addChild(layer);
+	return scene;
+};
+
+bool GameOver::init() {
+	if (!Layer::init()) {
+		return false;
+	}
+	//add back ground
+	auto size = Director::getInstance()->getWinSize();
+	auto spbk = Sprite::create("end.png");
+	spbk->setScale(0.5);
+	spbk->setPosition(Point(size.width / 2, size.height / 2));
+	this->addChild(spbk);
+	//add Back item
+	auto menuItemBack = MenuItemFont::create("Back", CC_CALLBACK_1(GameOver::menuCallBack, this));
+	menuItemBack->setColor(Color3B::BLACK);
+	auto menu = Menu::create(menuItemBack, NULL);
+	menu->setPosition(size.width - 50, 50);
+	this->addChild(menu);
+
+	return true;
+};
+
+void GameOver::menuCallBack(cocos2d::Ref * pSender) {
+	Director::getInstance()->replaceScene(TransitionJumpZoom::create(2, HelloWorld::createScene()));
+};
+
