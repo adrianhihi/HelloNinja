807f5509b4f737ed1ee42a93fdeeb3bb9ab3afdb
adrianhihi <adrianhihi@users.noreply.github.com>
2015-02-06 15:46:22 -0800

    Update GameOver.cpp

--- GameOver.cpp
+++ GameOver.cpp
@@ -17,14 +17,16 @@ bool GameOver::init() {
 	//add back ground
 	auto size = Director::getInstance()->getWinSize();
 	auto spbk = Sprite::create("end.png");
+	//this picture is too big for me, but suitable for you I think, so you can delete it.
 	spbk->setScale(0.5);
+	//put the background picture on the middle
 	spbk->setPosition(Point(size.width / 2, size.height / 2));
 	this->addChild(spbk);
 	//add Back item
 	auto menuItemBack = MenuItemFont::create("Back", CC_CALLBACK_1(GameOver::menuCallBack, this));
 	menuItemBack->setColor(Color3B::BLACK);
 	auto menu = Menu::create(menuItemBack, NULL);
-	menu->setPosition(size.width - 50, 50);
+	menu->setPosition(size.width * 0.5f, 50);
 	this->addChild(menu);
 
 	return true;
